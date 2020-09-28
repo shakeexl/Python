63d083f0f4b156a254a69bb7fc2cfc52b3de4fb5
shakee_xl <novikovroman96@gmail.com>
2020-09-28 23:14:20 +0300

    Загрузка заданий ко второму уроку

--- Exercise_2.py
+++ Exercise_2.py
@@ -1,11 +1,28 @@
 # Новиков Роман
 # Задание 2
-# Пользователь вводит время в секундах.
-# Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
-# Используйте форматирование строк.
+# Для списка реализовать обмен значений соседних элементов, т.е. Значениями обмениваются элементы
+# с индексами 0 и 1, 2 и 3 и т.д. При нечетном количестве элементов последний сохранить на своем месте.
+# Для заполнения списка элементов необходимо использовать функцию input().
 
-seconds_amount = input('Введите количество секунд: ')
-hours = int(seconds_amount) // 3600
-minutes = (int(seconds_amount)-(3600*hours)) // 60
-seconds = int(seconds_amount)-((3600*hours)+(60*minutes))
-print(f'Время: {hours}:{minutes}:{seconds}')
+my_list = []
+
+i = 0
+while len(my_list) < 7:  # Ограничение количества элементов листа
+    user_input = int(input('Введите какое-нибудь число: '))
+    my_list.insert(i, user_input)
+    i = i + 1
+my_new_list = my_list
+
+print(my_list)
+
+k = 0
+if len(my_list) % 2 == 0:
+    while k < (len(my_list)-1):
+        my_list[k], my_list[k + 1] = my_list[k + 1], my_list[k]
+        k = k + 2
+elif len(my_list) % 2 != 0:
+    while k < (len(my_list)-2):
+        my_list[k], my_list[k + 1] = my_list[k + 1], my_list[k]
+        k = k + 2
+
+print(my_list)

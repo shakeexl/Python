4af7d3caca5c850a8466c3e60a2f444070a25c60
shakee_xl <novikovroman96@gmail.com>
2020-10-05 19:17:49 +0300

    Загрузка заданий к четвертому уроку

--- Exercise_3.py
+++ Exercise_3.py
@@ -1,21 +1,6 @@
 # Новиков Роман
 # Задание 3
-# Реализовать функцию my_func(), которая принимает три позиционных аргумента,
-# и возвращает сумму наибольших двух аргументов.
+# Для чисел в пределах от 20 до 240 найти числа, кратные 20 или 21. Необходимо решить задание в одну строку.
 
-number_1 = int(input('Введите первое число: '))
-number_2 = int(input('Введите второе число: '))
-number_3 = int(input('Введите третье число: '))
-
-
-def my_func(a, b, c):
-    my_list = [a, b, c]
-    i = my_list.index(max(my_list))
-    m = my_list[i]
-    my_list.pop(i)
-    k = my_list.index(max(my_list))
-    n = my_list[k]
-    print('Сумма наибольших аргументов равна', m + n)
-
-
-my_func(number_1, number_2, number_3)
+number = [i for i in range (20, 240) if i % 20 == 0 or i % 21 == 0]
+print(number)
